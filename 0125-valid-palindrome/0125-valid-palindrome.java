3e345ad6005dbf302f6082449120dc42a1a2b20c
rohiiiit <68510500+rohiiiit@users.noreply.github.com>
2024-08-19 13:44:18 +0530

    Time: 2 ms (99.11%), Space: 43.1 MB (60.18%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i< str.length(); i++){
-            if(str.charAt(i)>= 97 && str.charAt(i)<= 122 || str.charAt(i)>=48 && str.charAt(i)<= 57){
-                sb.append(str.charAt(i));
+        int start = 0;
+        int end = s.length()-1;
+
+        while(start < end){
+            if(!Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            }else if(!Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            }else{
+                if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
+                    return false;
+                }
+                start++;
+                end--;
             }
-        }
-        s = sb.toString();
-        System.out.print(s);
-        int i = 0;
-        int j = s.length()-1;
-        while(i<j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-        
+        }    
+        return true;  
     }
 }
